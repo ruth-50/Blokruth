15426dbf31a34264264d293ef63f4d90b2e3b211
Ruth <gracielaruthg@gmail.com>
2020-05-17 20:14:04 -0300

    Cambie una variable y agregue una tabla

--- tablas.js
+++ tablas.js
@@ -1,7 +1,14 @@
 let dos = 2;
-let tabla;
-for (let i = 0; i < 10; i++) {
-    tabla =  dos * i;
-    console.log(`${dos} x ${i} = ${tabla}`);
+let tabla2;
+for (let i = 1; i < 10; i++) {
+    tabla2 =  dos * i;
+    console.log(`${dos} x ${i} = ${tabla2}`);
+    
+}
+let tres = 3;
+let tabla3;
+for (let i = 1; i < 10; i++) {
+    tabla3 = tres * i;
+    console.log(`${tres} x ${i} = ${tabla3}`);
     
 }
